76e8f85b922bbbaf29ceee38c95c2c5d36e58c4e
Carlos <cga.xtr@gmail.com>
2023-01-18 11:48:47 +0100

    problem 617 solved

---
+++ 617.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+int main(){
+
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+
+    int c;
+    std::cin >> c;
+
+    for(auto i = 0; i < c; i++){
+        int n;
+        std::cin >> n;
+        std::vector<std::string> v(n);
+
+        for(auto j = 0; j < n; j++){
+            std::cin >> v[j];
+        }
+
+        for(auto j = 0; j < 3; j++){
+            for(auto k = 0; k < n; k++)
+            std::cout << v[k][j];
+        }
+
+        std::cout << "\n";
+
+    }
+
+    return 0;
+}
