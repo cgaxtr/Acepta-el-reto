598eaf0c0d37f34d065b5639f244267bb5030cd9
Carlos <cga.xtr@gmail.com>
2023-02-08 21:25:14 +0100

    better solution

--- 482.cpp
+++ 482.cpp
@@ -1,38 +1,38 @@
 #include <iostream>
+#include <string>
 
-int main(){
+int main() {
 
 	std::ios::sync_with_stdio(false);
 	std::cin.tie(nullptr);
 
-	char aux;
-	std::cin >> aux;
-	while(aux != '.'){
+	std::string s;
+	std::getline(std::cin, s);
+	while (s != ".") {
 
 		auto nCounter = 0;
 		auto gCounter = 0;
 
-		while(aux != '.'){
-			if(aux == 'N')
+		for (auto i = 0; i < s.length(); i = i + 2) {
+			if(s[i] == 'N')
 				++nCounter;
-			else
+			else if(s[i] == 'G')
 				++gCounter;
-			std::cin >> aux;
 		}
 
 		bool flagN = nCounter % 2 == 0;
 		bool flagG = gCounter % 2 == 0;
 
-		if(flagN && flagG)
+		if (flagN && flagG)
 			std::cout << "EMPAREJADOS\n";
-		else if(flagN && !flagG)
+		else if (flagN && !flagG)
 			std::cout << "GRIS SOLITARIO\n";
-		else if(!flagN && flagG)
+		else if (!flagN && flagG)
 			std::cout << "NEGRO SOLITARIO\n";
 		else
 			std::cout << "PAREJA MIXTA\n";
 
-		std::cin >> aux;
+		std::getline(std::cin, s);
 	}
 
 	return 0;
